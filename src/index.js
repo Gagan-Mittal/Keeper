9d2352159b89eca5a7f41f893cdd69de8848bfe2
Gagan-Mittal <81302027+Gagan-Mittal@users.noreply.github.com>
2021-12-02 17:45:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./component/App";
-// testing part 2
+// testing part 3
 ReactDom.render( <App/> ,document.getElementById("root"));
